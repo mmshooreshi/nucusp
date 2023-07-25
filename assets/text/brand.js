179714069ab8844ff93f84786108caa5d131d5dc
mmshooreshi <mmshooreshi@gmail.com>
2023-07-25 08:16:14 +0330

    s

---
+++ assets/text/brand.js
@@ -0,0 +1,15 @@
+const brand = {
+  mobile: {
+    name: "Mobile",
+    desc: "Nucusp",
+    prefix: "Nucusp",
+    footerText: "Nucusp Company",
+    logoText: "Nucusp co.",
+    projectName: "Mobile",
+    url: "nucusp.com/mobile",
+    img: "/images/mobile-logo.png",
+    notifMsg: "...",
+  },
+};
+
+export default brand;
