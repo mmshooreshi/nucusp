3986d581013c2e96e804d891e484719bb7124d1e
mmshooreshi <mmshooreshi@gmail.com>
2023-07-25 10:01:35 +0330

    lang

--- config/theme.js
+++ config/theme.js
@@ -1,15 +1,17 @@
 // Colors
-import colors from 'vuetify/lib/util/colors';
+import colors from "vuetify/lib/util/colors";
 
 const palette = {
   joker: {
-    primary: colors.deepPurple.base, // primary main
+    //primary: colors.deepPurple.base, // primary main
+
+    primary: colors.deepPurple.darken4,
     primarylight: colors.deepPurple.lighten4, // primary light
     primarydark: colors.deepPurple.darken4, // primary dark
     secondary: colors.cyan.base, // secondary main
     secondarylight: colors.cyan.lighten4, // secondary light
     secondarydark: colors.cyan.darken4, // secondary dark
-    anchor: colors.deepPurple.base // link
+    anchor: colors.deepPurple.base, // link
   },
 };
 
